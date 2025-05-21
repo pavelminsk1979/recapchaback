426e2c59d512177bb8a53941983b232d85bb5cbc
pavvelpotapov <pavelminsk1979@mail.ru>
2025-05-21 20:12:52 +0300

    com2

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,6 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
+import axios from 'axios';
 
 @Controller()
 export class AppController {
@@ -11,10 +12,19 @@ export class AppController {
   }
 
   @Post()
-  postTrain(@Body() body: { value: string; title: string }) {
-    const { value, title } = body;
+  async postTrain(
+    @Body() body: { value: string; title: string; token: string },
+  ) {
+    const { value, title, token } = body;
     console.log('body', value);
     console.log('body', title);
+    console.log('token', token);
+
+    const resultFromGoogle = await axios.post(
+      'https://www.google.com/recaptcha/api/siteverify',
+      `secret=6LcIaUMrAAAAAGQeSe5nD4bOBayg188byNV-NGoe&response=${token}`,
+    );
+    console.log('resultFromGoogle', resultFromGoogle.data);
 
     return '11111';
   }
